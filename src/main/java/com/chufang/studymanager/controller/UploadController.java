2f22385dd7fdcdf6d5446ee56704583a30aa3bf3
likunpeng <360315247@qq.com>
2023-07-24 01:26:14 +0800

    完善文件上传

--- src/main/java/com/chufang/studymanager/controller/UploadController.java
+++ src/main/java/com/chufang/studymanager/controller/UploadController.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
     @PostMapping("/upload")
-    public Result upload(String username, Integer age, MultipartFile image) {
+    public Result upload(String username, Integer age, MultipartFile image) throws IOException {
         log.info("上传文件信息 username = {}, age = {}, image = {}", username, age, image);
+        String originalFilename = image.getOriginalFilename();
+        // 构建唯一的文件名
+        String newFileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+        image.transferTo(new File("/Users/lkp/Documents/" + newFileName));
         return Result.success();
     }
 }
